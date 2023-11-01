bc42bf06d9f156c686f83a914588a71209c143cd
Kamil <82053152+Kasy00@users.noreply.github.com>
2023-11-01 18:32:24 +0100

    Added classes to digits buttons and added display, clearing display

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const digitsBtns = document.querySelectorAll('.digit-button');
+const operandsBtns = document.querySelectorAll('.operands-button');
+const display = document.querySelector('.display');
+const previousScreen = document.querySelector('.previous');
+const currentScreen = document.querySelector('.current');
+const equalsBtn = document.querySelector('.equals-button');
+const clearBtn = document.getElementById('clear-button');
+
 const add = function(a, b){
     return a + b;
 }
@@ -14,8 +22,8 @@ const divide = function(a, b){
     return b !== 0 ? a / b : alert('Cannot divide by zero!');
 }
 
-let aNumber = '',
-    bNumber = '',
+let aNumber = [],
+    bNumber = [],
     operator = '';
 
 const operate = function(operator, firstNumber, secondNumber){
@@ -30,3 +38,41 @@ const operate = function(operator, firstNumber, secondNumber){
     else
         return "Incorrect operator!";
 }
+
+const displayScreen = function(button){
+    if(operator.length === 0){
+        aNumber.push(button.textContent);
+        previousScreen.textContent = aNumber.join('');
+        console.log('aNumber:', aNumber);
+    }
+    else{
+        bNumber.push(button.textContent);
+        currentScreen.textContent = bNumber.join('');
+        console.log('bNumber:', bNumber);
+    }
+}
+
+const setOperator = function(button){
+    if(button.textContent === '+')
+        operator = '+';
+    else if(button.textContent === '-')
+        operator = '-';
+    else if(button.textContent === '×')
+        operator = '*';
+    else
+        operator = '/';
+}
+
+const clearDisplay = function(){
+    aNumber = [];
+    bNumber = [];
+    operator = '';
+    previousScreen.innerHTML = '';
+    currentScreen.innerHTML = '';
+}
+
+digitsBtns.forEach(button => button.addEventListener("click", () => displayScreen(button)));
+operandsBtns.forEach(button => button.addEventListener("click", () => setOperator(button)));
+clearBtn.addEventListener('click', clearDisplay);
+
+
