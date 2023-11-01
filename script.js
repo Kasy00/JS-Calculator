b4691ee60f1d1b23f0eaffd36af258430e15b057
Kamil <82053152+Kasy00@users.noreply.github.com>
2023-11-01 12:05:58 +0100

    Added operate function that calls one of the functions on the numbers

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ const divide = function(a, b){
     return b !== 0 ? a / b : alert('Cannot divide by zero!');
 }
 
+let aNumber = '',
+    bNumber = '',
+    operator = '';
+
+const operate = function(operator, firstNumber, secondNumber){
+    if(operator === '+')
+        return add(firstNumber, secondNumber);
+    else if(operator === '-')
+        return subtract(firstNumber, secondNumber);
+    else if(operator === '*')
+        return multiply(firstNumber, secondNumber);
+    else if(operator === '/')
+        return divide(firstNumber, secondNumber);
+    else
+        return "Incorrect operator!";
+}
