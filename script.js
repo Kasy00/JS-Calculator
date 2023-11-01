6a13d23e34429bd49ad6d8f612fac084ebdad693
Kamil <82053152+Kasy00@users.noreply.github.com>
2023-11-01 18:58:15 +0100

    Added result displaying after clicking on equals button

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const operandsBtns = document.querySelectorAll('.operands-button');
 const display = document.querySelector('.display');
 const previousScreen = document.querySelector('.previous');
 const currentScreen = document.querySelector('.current');
-const equalsBtn = document.querySelector('.equals-button');
+const equalsBtn = document.getElementById('equals-button');
 const clearBtn = document.getElementById('clear-button');
 
 const add = function(a, b){
@@ -26,14 +26,17 @@ let aNumber = [],
     bNumber = [],
     operator = '';
 
-const operate = function(operator, firstNumber, secondNumber){
-    if(operator === '+')
+const operate = function(operand, firstNumber, secondNumber){
+    console.log(operand);
+    console.log(firstNumber);
+    console.log(secondNumber);
+    if(operand === '+')
         return add(firstNumber, secondNumber);
-    else if(operator === '-')
+    else if(operand === '-')
         return subtract(firstNumber, secondNumber);
-    else if(operator === '*')
+    else if(operand === '*')
         return multiply(firstNumber, secondNumber);
-    else if(operator === '/')
+    else if(operand === '/')
         return divide(firstNumber, secondNumber);
     else
         return "Incorrect operator!";
@@ -43,12 +46,10 @@ const displayScreen = function(button){
     if(operator.length === 0){
         aNumber.push(button.textContent);
         previousScreen.textContent = aNumber.join('');
-        console.log('aNumber:', aNumber);
     }
     else{
         bNumber.push(button.textContent);
         currentScreen.textContent = bNumber.join('');
-        console.log('bNumber:', bNumber);
     }
 }
 
@@ -61,18 +62,25 @@ const setOperator = function(button){
         operator = '*';
     else
         operator = '/';
+    previousScreen.textContent += operator;
 }
 
 const clearDisplay = function(){
     aNumber = [];
     bNumber = [];
     operator = '';
-    previousScreen.innerHTML = '';
-    currentScreen.innerHTML = '';
+    previousScreen.textContent = '';
+    currentScreen.textContent = '';
+}
+
+const getResult = function(){
+    const result = operate(operator, Number(aNumber.join('')), Number(bNumber.join('')));
+    currentScreen.textContent = '';
+    previousScreen.textContent = result;
 }
 
 digitsBtns.forEach(button => button.addEventListener("click", () => displayScreen(button)));
 operandsBtns.forEach(button => button.addEventListener("click", () => setOperator(button)));
 clearBtn.addEventListener('click', clearDisplay);
-
+equalsBtn.addEventListener('click', getResult);
 
